c78ca4d176ba56a1ba1897196edb63558c39e0c5
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-07-09 18:00:01 +0530

    setup basic container for application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Container, Stack, Button } from 'react-bootstrap'
 
 export default function App() {
   return (
-    <div>Hello world</div>
+    <Container className="my-4">
+      <Stack 
+      direction="horizontal" 
+      gap="2"
+      className="mb-4"
+      >
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      <div style={
+        {
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+          gap: "1rem",
+          alignItems: "flex-start"
+    
+        }
+      }>
+      </div>
+    </Container>
   )
 }
